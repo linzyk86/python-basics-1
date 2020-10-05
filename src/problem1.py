3dd3bb19ab77fe42150a14e9d5c49470e4a4f384
Lindsay W <lwhite5314@gmail.com>
2020-10-05 08:30:09 -0400

    problems 1-3

---
+++ src/problem1.py
@@ -0,0 +1,12 @@
+
+def split_in_parts(s, part_length):
+    # Your code here
+    #create a variable for the new string
+    return_list = []
+    #loop through
+    for letter in s:
+        print(letter)
+    #string_slice = s
+split_in_parts("supercalifragilisticexpialidocious", 3)
+################
+
---
+++ src/problem2.py
@@ -0,0 +1,15 @@
+
+
+
+def oddNumbersBeforeZero(sequence):
+
+    result = 0
+    for i in range(0, len(sequence)):
+        if sequence[i] == 0:
+            break
+        if sequence[i] % 2 == 1:
+            result += 1
+        print(result)
+
+oddNumbersBeforeZero([1, 2, 3, 0, 4, 5, 6, 0, 1])
+print("blah")
